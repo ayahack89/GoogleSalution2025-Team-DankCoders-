5d04dfedc93f02cab3208ee746a7f7aa141b9973
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2025-03-26 09:47:45 +0530

    Update

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\91987\Documents\GitHub\GoogleSalution2025-Team-DankCoders-\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
