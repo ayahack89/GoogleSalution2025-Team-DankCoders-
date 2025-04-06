2742b203ba151d578ca944a35ce9b75852d45029
Agnik Roy <102238499+Ar12agnik@users.noreply.github.com>
2025-04-07 00:08:46 +0530

    update

--- myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\91987\Documents\GitHub\GoogleSalution2025-Team-DankCoders-\myenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
